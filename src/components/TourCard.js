ff267146a068287fc287474be0fb4759dad746f6
seungyong88 <gosng2001@naver.com>
2022-03-27 03:51:53 +0900

    first commit

---
+++ src/components/TourCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Paper from '@mui/material/Paper';
+
+function TourCard() {
+  return (
+    <Paper elevation={2}>
+      Hello
+    </Paper>
+  )
+}
+
+export default TourCard
